2a5abaa91acb2865779131906e0d4eec3167b3b8
ajitkute <103120689+ajitkute@users.noreply.github.com>
2022-04-07 23:31:08 +0530

    Add files via upload

---
+++ Large_No.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class Large_No {
+
+	public static void main(String[] args) {
+
+		int n1,n2,n3;
+		n1=10; n2=20; n3=30;
+		if(n1>n2 && n1>n3)
+			System.out.println(n1+" is larger");
+		else if (n2>n1 && n2>n3)
+			System.out.println(n2+" is larger");
+		else
+			System.out.println(n3+" is larger");
+
+	}
+
+}
